d9749bddc8d69d066af362f228f34577b33b1769
mglvarella <mig-varella1@hotmail.com>
2025-09-11 21:46:14 -0300

    Consertando as rotas para seguir o padrão restFul

--- src/main/java/com/mglvarella/listadecompras/controller/ProductController.java
+++ src/main/java/com/mglvarella/listadecompras/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/products") // ALTERADO: singular para plural ("/product" -> "/products")
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
