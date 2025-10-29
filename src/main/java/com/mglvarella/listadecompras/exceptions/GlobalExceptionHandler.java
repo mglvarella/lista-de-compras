24ac4806f8b69a50462be499636735d4daf83a19
mglvarella <mig-varella1@hotmail.com>
2025-10-29 00:26:30 -0300

    Ajuste no handler de bad request

--- src/main/java/com/mglvarella/listadecompras/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mglvarella/listadecompras/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.mglvarella.listadecompras.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
